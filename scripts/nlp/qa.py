9b4136c485fe23befafade2acc66db63b6645abe
Michael Elliott <mielliott@ufl.edu>
2024-01-22 16:56:50 +0000

    Use updated openai API

--- scripts/nlp/qa.py
+++ scripts/nlp/qa.py
@@ -10,11 +10,10 @@ import argparse
 import os
 import sys
 import time
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-from torch.nn.functional import softmax
-
-import openai
-openai.api_key = os.getenv("OPENAI_API_KEY")
+from openai import OpenAI
+from openai.types.chat import ChatCompletion
+# from transformers import T5Tokenizer, T5ForConditionalGeneration
+# from torch.nn.functional import softmax
 
 def unescape(string):
     if '"' == string[0] == string[-1] or "'" == string[0] == string[-1]:
@@ -37,46 +36,45 @@ def get_questions(patterns, header, lines, do_unescape, filter=lambda x: True):
                 field_values = dict(zip(fields, values))
                 yield (line, pattern.format(**field_values))
 
-def prep_local_model():
-    global tokenizer, model, qa
-    tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
-    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", device_map="auto")
+# def prep_local_model():
+#     global tokenizer, model, qa
+#     tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
+#     model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", device_map="auto")
 
-    def qa(question):
-        i = tokenizer(question, return_tensors="pt", padding=True).input_ids.to("cuda")
-        o = model.generate(i, max_new_tokens=200, return_dict_in_generate=True, output_scores=True)
-        print(tokenizer.decode(o.sequences[0], skip_special_tokens=True))
-
+#     def qa(question):
+#         i = tokenizer(question, return_tensors="pt", padding=True).input_ids.to("cuda")
+#         o = model.generate(i, max_new_tokens=200, return_dict_in_generate=True, output_scores=True)
+#         print(tokenizer.decode(o.sequences[0], skip_special_tokens=True))
 
 def batched(iterable, n):
     args = [iter(iterable)] * n
     return zip(*args)
 
-def run_local_model():
-    print("response", "tokens", "log scores", "softmax scores", sep="\t")
+# def run_local_model():
+#     print("response", "tokens", "log scores", "softmax scores", sep="\t")
 
-    batch_size = 4
-    for batch in batched(questions, batch_size):
-        i = tokenizer(batch, return_tensors="pt", padding=True).input_ids.to("cuda")
-        o = model.generate(i, max_new_tokens=20, return_dict_in_generate=True, output_scores=True)
+#     batch_size = 4
+#     for batch in batched(questions, batch_size):
+#         i = tokenizer(batch, return_tensors="pt", padding=True).input_ids.to("cuda")
+#         o = model.generate(i, max_new_tokens=20, return_dict_in_generate=True, output_scores=True)
         
-        for iseq, seq in enumerate(o.sequences):
-            response = seq[1:-1]
-            log_scores = [o.scores[it][iseq][t] for it, t in enumerate(response)]
-            softmax_scores = [softmax(o.scores[it][iseq], dim=0)[t] for it, t in enumerate(response)]
+#         for iseq, seq in enumerate(o.sequences):
+#             response = seq[1:-1]
+#             log_scores = [o.scores[it][iseq][t] for it, t in enumerate(response)]
+#             softmax_scores = [softmax(o.scores[it][iseq], dim=0)[t] for it, t in enumerate(response)]
 
-            print(tokenizer.decode(response),
-                " ".join([str(id) for id in response.tolist()]),
-                " ".join(["{:.3f}".format(score.tolist()) for score in log_scores]),
-                " ".join(["{:.3f}".format(score.tolist()) for score in softmax_scores]),
-                sep="\t",
-                flush=True)
+#             print(tokenizer.decode(response),
+#                 " ".join([str(id) for id in response.tolist()]),
+#                 " ".join(["{:.3f}".format(score.tolist()) for score in log_scores]),
+#                 " ".join(["{:.3f}".format(score.tolist()) for score in softmax_scores]),
+#                 sep="\t",
+#                 flush=True)
 
 # For kwargs see https://platform.openai.com/docs/api-reference/chat/create
-def query_openai(model: str, query: str, **kwargs) -> list[str]:
+def query_openai(client: OpenAI, model: str, query: str, **kwargs) -> ChatCompletion:
     while (True):
         try:
-            response = openai.ChatCompletion.create(
+            response = client.chat.completions.create(
                 model=model,
                 messages=[{"role": "user", "content": query}],
                 **kwargs
@@ -88,12 +86,14 @@ def query_openai(model: str, query: str, **kwargs) -> list[str]:
             time.sleep(5)
 
 def run_openai_model(model, header_in, max_tokens, num_responses, top_p, combine_responses, timeout, escape):
+    client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+
     print(header_in, "query", "responses", "input token count", "output token count", "question number", sep="\t")
 
     for i, (input, question) in enumerate(questions):
         # See https://platform.openai.com/docs/api-reference/chat/create
-        response = query_openai(model, question, n=num_responses, top_p=top_p, max_tokens=max_tokens, request_timeout=timeout)
-        answers = [choice["message"]["content"] for choice in response["choices"]]
+        response = query_openai(client, model, question, n=num_responses, top_p=top_p, max_tokens=max_tokens, timeout=timeout)
+        answers = [choice.message.content for choice in response.choices]
         if combine_responses:
             answers = [" ".join(answers)]
         for answer in answers:
