1f10f4b0cab7b4a721d692662b5c879f5bac605f
Michael Elliott <mielliott@ufl.edu>
2024-11-07 19:54:35 +0000

    Add a realistic test

--- scripts/nlp/tests/test_runner.py
+++ scripts/nlp/tests/test_runner.py
@@ -29,6 +29,29 @@ def test_runner():
     )
 
 
+def test_runner_with_disk_io():
+    io_handler = IOHandler(
+        ["Does species {genus} {specificepithet} live in {country}?"],
+        False,
+        []
+    )
+
+    runner = ExperimentRunner("test", {}, io_handler)
+
+    out_stream = io.StringIO()
+
+    with open("tests/resources/occurrence-qa.tsv", "r", encoding="utf-8") as f:
+        runner.run_experiment(f, out_stream)
+
+    out_tsv = out_stream.getvalue()
+
+    assert out_tsv == (
+        "genus\tspecificepithet\tcountry\tquery\tresponse\n"
+        "Panicum\tlaxiflorum\tNepal\tDoes species Panicum laxiflorum live in Nepal?\tEchoing query \\\"Does species Panicum laxiflorum live in Nepal?\\\"\n"
+        "Fenestella\tcarinata\tUganda\tDoes species Fenestella carinata live in Uganda?\tEchoing query \\\"Does species Fenestella carinata live in Uganda?\\\"\n"
+    )
+
+
 def test_runner_missing_required_fields():
     tsv = test_util.make_tsv_stream([{"x": "apple", "y": "orange"}, {"x": "horse", "y": "carriage"}])
     io_handler = IOHandler(
