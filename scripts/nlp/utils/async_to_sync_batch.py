748610ac643dd4612db134d6fcd4254e25b5603e
Nitin Goyal <nitin.goyal@ufl.edu>
2024-12-02 19:34:48 +0000

    cleanup async to sync script

--- scripts/nlp/utils/async_to_sync_batch.py
+++ scripts/nlp/utils/async_to_sync_batch.py
@@ -6,14 +6,11 @@ from aiohttp import ClientSession
 import json
 import aiohttp
 
-# create an asyncio loop that runs in the background to
-# serve our asyncio needs
-loop = asyncio.get_event_loop()
-thread = threading.Thread(target=loop.run_forever, daemon=True)
-thread.start()
 
+class RunStatus:
+    running = True
 
-def wrap_async_iter(ait):
+def wrap_async_iter(ait, loop):
     """Wrap an asynchronous iterator into a synchronous one"""
     q = queue.Queue()
     _END = object()
@@ -38,18 +35,6 @@ def wrap_async_iter(ait):
 
     async_result = asyncio.run_coroutine_threadsafe(aiter_to_queue(), loop)
     return yield_queue_items()
-
-
-class RunStatus:
-    def __init__(self, status):
-        self.status = status
-
-    def get_running(self):
-        return self.status
-
-    def set_running(self, status):
-        self.status = status
-
 
 async def create_req_object(queries, queue):
     for idx, query in enumerate(queries):
@@ -78,12 +63,12 @@ async def upload_to_openai(file_like_obj, queue, run_status_obj):
         data.add_field('file', file_like_obj, filename='batch.jsonl')
         async with session.post(url, headers=headers, data=data) as response:
             await queue.put(('response', str(response)))
-            run_status_obj.set_running(False)
+            run_status_obj.running = False
             return await response.json()
 
 
 async def flush_queue(queue, run_status_obj):
-    while run_status_obj.get_running():
+    while run_status_obj.running:
         item = await queue.get()
         yield {
             "request id": item[0],
@@ -104,11 +89,17 @@ def data_file():
     return iter(['this is a random data string which is going to be written to a file', 'this is a second line of data', 'this is a third line of data', 'this is a fourth line of data', 'this is a fifth line of data', 'this is a sixth line of data', 'this is a seventh line of data', 'this is a eighth line of data', 'this is a ninth line of data', 'this is a tenth line of data', 'this is an eleventh line of data', 'this is a twelfth line of data', 'this is a thirteenth line of data', 'this is a fourteenth line of data', 'this is a fifteenth line of data', 'this is a sixteenth line of data', 'this is a seventeenth line of data', 'this is a eighteenth line of data', 'this is a nineteenth line of data', 'this is a twentieth line of data', 'this is a twenty-first line of data', 'this is a twenty-second line of data', 'this is a twenty-third line of data', 'this is a twenty-fourth line of data', 'this is a twenty-fifth line of data', 'this is a twenty-sixth line of data', 'this is a twenty-seventh line of data', 'this is a twenty-eighth line of data', 'this is a twenty-ninth line of data', 'this is a thirtieth line of data', 'this is a thirty-first line of data', 'this is a thirty-second line of data', 'this is a thirty-third line of data', 'this is a thirty-fourth line of data', 'this is a thirty-fifth line of data', 'this is a thirty-sixth line of data', 'this is a thirty-seventh line of data', 'this is a thirty-eighth line of data', 'this is a thirty-ninth line of data', 'this is a fortieth line of data', 'this is a forty-first line of data', 'this is a forty-second line of data', 'this is a forty-third line of data', 'this is a forty-fourth line of data', 'this is a forty-fifth line of data', 'this is a forty-sixth line of data', 'this is a forty-seventh line of data', 'this is a forty-eighth line of data', 'this is a forty-ninth line of data', 'this is a fiftieth line of data'])
 
 
+# create an asyncio loop that runs in the background to
+# serve our asyncio needs
+loop = asyncio.get_event_loop()
+thread = threading.Thread(target=loop.run_forever, daemon=True)
+thread.start()
+
 item_queue = asyncio.Queue(10)
 data = data_file()
-run_status_obj = RunStatus(True)
+run_status_obj = RunStatus()
 asyncio.run_coroutine_threadsafe(create_batch(data, item_queue, run_status_obj), loop)
-for item in wrap_async_iter(flush_queue(item_queue, run_status_obj)):
+for item in wrap_async_iter(flush_queue(item_queue, run_status_obj), loop):
     if item["request id"] == 'response':
         continue
     print(item)
