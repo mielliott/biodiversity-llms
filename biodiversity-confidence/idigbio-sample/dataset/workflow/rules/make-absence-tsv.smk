74354682ff4abc64172ec18b7a3e569234e649f1
Michael Elliott <mielliott@ufl.edu>
2024-12-17 21:25:29 +0000

    Rework absence.tsv

--- biodiversity-confidence/idigbio-sample/dataset/workflow/rules/make-absence-tsv.smk
+++ biodiversity-confidence/idigbio-sample/dataset/workflow/rules/make-absence-tsv.smk
@@ -2,52 +2,20 @@ rule create_pseudo_absence_dataset:
     input:
         "results/presence.tsv",
     output:
-        temp("results/absence.tsv.unvalidated"),
-    log:
-        "logs/absence.tsv.unvalidated.tsv",
-    params:
-        shuffle_fields="country,stateprovince,county",
-        seed=config["random_seed"],
-    shell:
-        """
-        paste <(cat {input} | mlr --tsv cut -x -f {params.shuffle_fields})\
-              <(cat {input} | mlr --tsv cut -f {params.shuffle_fields} | mlr --tsv --seed {params.seed} shuffle)\
-        > {output}
-        """
-
-
-rule validate_absences:
-    input:
-        f"results/absence.tsv.unvalidated",
-    output:
-        f"results/absence-valid.tsv",
-    log:
-        "logs/absence-valid.tsv",
-    conda:
-        "../envs/download.yml"
-    params:
-        idigbio_api=config["idigbio_api"],
-    shell:
-        """
-        cat {input}\
-        | python3 workflow/scripts/validate_absences.py {params.idigbio_api}\
-        1> {output} 2> {log}
-        """
-
-
-rule filter_absences:
-    input:
-        unvalidated=f"results/absence.tsv.unvalidated",
-        validation=f"results/absence-valid.tsv",
-    output:
         f"results/absence.tsv",
     log:
         "logs/absence.tsv",
+    conda:
+        "../envs/download.yml"
+    params:
+        location_fields="country,stateprovince,county",
+        max_retries=100,
+        idigbio_api=config["idigbio_api"],
+        random_seed=config["random_seed"],
     shell:
         """
-        paste <(cat {input.unvalidated})\
-              <(cut -f2 {input.validation})\
-        | mlr --tsvlite filter '$valid == "True"'\
-        | mlr --tsvlite cut -xf "valid"\
-        > {output}
+        mlr --tsv cut -x -f {params.location_fields} {input}\
+        | python3 workflow/scripts/make_absences.py {input} {params.location_fields} {params.max_retries} {params.idigbio_api} {params.random_seed}\
+        | mlr --tsv uniq -a\
+        1> {output} 2> {log}
         """
---
+++ biodiversity-confidence/idigbio-sample/dataset/workflow/scripts/make_absences.py
@@ -0,0 +1,68 @@
+import csv
+import itertools
+import sys
+from typing import Any, Sequence, cast
+import concurrent.futures
+import pandas as pd
+import requests as rq
+
+args = iter(sys.argv[1:])
+
+LOCATIONS_PATH = str(next(args))
+LOCATION_FIELDS = str(next(args))
+MAX_RETRIES = int(next(args))
+API = str(next(args))
+RANDOM_STATE = int(next(args))
+
+location_fields = LOCATION_FIELDS.split(",")
+locations = pd.read_csv(LOCATIONS_PATH, sep="\t", usecols=location_fields)
+
+
+def make_species_location_query(record):
+    return {
+        "rq": {
+            "kingdom": str(record["kingdom"]),
+            "genus": str(record["genus"]),
+            "specificepithet": str(record["specificepithet"]),
+            "country": str(record["country"]),
+            "stateprovince": str(record["stateprovince"]),
+            # "county": str(record["county"]), # County names do not match reliably, stateprovince is good enough
+        },
+        "limit": 1,
+        "offset": 0,
+    }
+
+
+tsv_args: dict[str, Any] = dict(
+    delimiter="\t",
+    lineterminator="\n",
+    escapechar="\\",
+    quoting=csv.QUOTE_NONE
+)
+
+records = csv.DictReader(sys.stdin, **tsv_args)
+
+taxonomy_fields = list(records.fieldnames) if records.fieldnames is not None else []
+fields = taxonomy_fields + location_fields
+
+validated_absences = csv.DictWriter(sys.stdout, fields, **tsv_args)
+validated_absences.writeheader()
+
+random_box = [RANDOM_STATE]
+
+
+def make_pseudo_absence(record):
+    random_box[0] += 1
+    for _, location in locations.sample(MAX_RETRIES, random_state=random_box[0]).iterrows():
+        absence_record = record | location.to_dict()
+        query = make_species_location_query(absence_record)
+        response = rq.post(f"{API}/search/records", json=query)
+        if response.json()["itemCount"] == 0:
+            return absence_record
+    return None
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+    for absence_record in executor.map(make_pseudo_absence, records):
+        if absence_record is not None:
+            validated_absences.writerow(absence_record)
